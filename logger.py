4220ca4af13697e7e5f3d43adbc6d6e2077175d7
Michal Labowski <michal.labowski@gmail.com>
2019-08-10 13:13:40 +0200

    fix: Formatting in the __init__method

--- logger.py
+++ logger.py
@@ -6,21 +6,21 @@ class Logger:
     def __init__(self, logger_name='__name__', file_name='app.log',
                  file_level=logging.DEBUG, console_level=logging.INFO,
                  formatter='%(created).6f %(name)-12s %(levelname)-8s %(message)s'):
-            self.logger = logging.getLogger(logger_name)
-            self.logger.setLevel(logging.DEBUG)
-            
-            self.console_formatter = logging.Formatter(formatter)
-            self.console_level = console_level
-            self.init_console_handler()
-            self.logger.addHandler(self.console_handler)
+        self.logger = logging.getLogger(logger_name)
+        self.logger.setLevel(logging.DEBUG)
+        
+        self.console_formatter = logging.Formatter(formatter)
+        self.console_level = console_level
+        self.init_console_handler()
+        self.logger.addHandler(self.console_handler)
 
-            self.file_name = file_name
-            self.file_level = file_level
-            self.file_formatter = logging.Formatter(formatter)
-            self.init_file_handler()
-            self.logger.addHandler(self.file_handler)
+        self.file_name = file_name
+        self.file_level = file_level
+        self.file_formatter = logging.Formatter(formatter)
+        self.init_file_handler()
+        self.logger.addHandler(self.file_handler)
 
-            self.logger.propagate = False
+        self.logger.propagate = False
             
 
     def init_console_handler(self):
