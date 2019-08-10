0b15dd6e306f1ec6b2c630cedc9dfc4205044e31
Michal Labowski <michal.labowski@gmail.com>
2019-08-10 13:12:25 +0200

    feat: Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Python Logger Class
+A class uses python logging module. 
+It allows to simoltanously write log messages to log file and to the console.
+The levels for the file and the console can be determined by contructor,
+as well as the output file name. The constructor allows to change the default
+formatter. In default formatter the time is unix time (seconds.microseconds).
+
+## How to use
+
+In this example a logger object is created, where:
+* 'my_cute.log' is the name of the output file
+* 'logging.DEBUG' is the level set to the file output
+* 'logging.INFO' is the level set to the console output
+
+'''
+logger = Logger(__name__, 'my_cute.log', logging.DEBUG, logging.INFO)
+logger.debug('A debug message')
+logger.info('An info message')
+logger.warning('A warning message')
+logger.error('An error message')
+logger.critical('A critical message')
+'''
+
+## License
+The contents of this repository are covered under the [MIT License](./LICENSE.txt)
---
+++ logger.py
@@ -0,0 +1,65 @@
+import logging
+import sys
+import time
+
+class Logger:
+    def __init__(self, logger_name='__name__', file_name='app.log',
+                 file_level=logging.DEBUG, console_level=logging.INFO,
+                 formatter='%(created).6f %(name)-12s %(levelname)-8s %(message)s'):
+            self.logger = logging.getLogger(logger_name)
+            self.logger.setLevel(logging.DEBUG)
+            
+            self.console_formatter = logging.Formatter(formatter)
+            self.console_level = console_level
+            self.init_console_handler()
+            self.logger.addHandler(self.console_handler)
+
+            self.file_name = file_name
+            self.file_level = file_level
+            self.file_formatter = logging.Formatter(formatter)
+            self.init_file_handler()
+            self.logger.addHandler(self.file_handler)
+
+            self.logger.propagate = False
+            
+
+    def init_console_handler(self):
+        self.console_handler = logging.StreamHandler(sys.stdout)
+        self.console_handler.setFormatter(self.console_formatter)
+        self.console_handler.setLevel(self.console_level)
+    
+
+    def init_file_handler(self):
+        self.file_handler = logging.FileHandler(self.file_name)
+        self.file_handler.setFormatter(self.file_formatter)
+        self.file_handler.setLevel(self.file_level)
+
+
+    def debug(self, msg):
+        self.logger.debug(msg)
+
+
+    def info(self, msg):
+        self.logger.info(msg)
+    
+
+    def warning(self, msg):
+        self.logger.warning(msg)
+        
+
+    def error(self, msg):
+        self.logger.error(msg)
+
+
+    def critical(self, msg):
+        self.logger.critical(msg)
+
+
+logger = Logger(__name__, 'my_cute.log', logging.DEBUG, logging.INFO)
+logger.debug('A debug message')
+logger.info('An info message')
+logger.warning('A warning message')
+logger.error('An error message')
+logger.critical('A critical message')
+
+
