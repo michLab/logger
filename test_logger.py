49ba9c53a56e8908db6a0638ec31581e6baeada9
Michal Labowski <michal.labowski@gmail.com>
2019-09-28 16:06:55 +0200

    feat: Add thread-safe example

--- test_logger.py
+++ test_logger.py
@@ -1,13 +1,15 @@
+import threading
+from time import sleep
 import logger
 
 my_logger = logger.Logger(logger_name=__name__, file_name="./test.log",
                           console_level=logger.INFO, file_level=logger.DEBUG)
 message_number = 1
-my_logger.info('This it a test info message with num %d', message_number)
-my_logger.debug('This it a test debug message %s', str(message_number))
-my_logger.warning('This it a test warning message')
-my_logger.error('This it a test error message')
-my_logger.critical('This it a test critical message')
+my_logger.info('This is a test info message with num %d', message_number)
+my_logger.debug('This is a test debug message %s', str(message_number))
+my_logger.warning('This is a test warning message')
+my_logger.error('This is a test error message')
+my_logger.critical('This is a test critical message')
 
 a = 5
 b = 0
@@ -16,3 +18,28 @@ try:
 except Exception as e:
     my_logger.exception("Exception occured")
 
+logger_with_thread = logger.Logger(logger_name=__name__, file_name="./test_thread.log",
+                          console_level=logger.INFO, file_level=logger.DEBUG)
+
+def producer_callback(name="Default producer", arg_logger=None):
+    logger = arg_logger
+    while True:
+        if logger:
+            logger.info('[%s]: This is a test info message', name)
+            sleep(0.001)
+        else:
+            return 
+
+producer_A = threading.Thread(target=producer_callback, kwargs=dict(arg_logger=logger_with_thread,
+                                                                    name="Producer A"))
+producer_A.daemon=True
+producer_A.start()
+
+producer_B = threading.Thread(target=producer_callback, kwargs=dict(arg_logger=logger_with_thread,
+                                                                    name="Producer B"))
+producer_B.daemon=True
+producer_B.start()
+
+while True:
+    print("Still running...")
+    sleep(10)
