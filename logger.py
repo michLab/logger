944e8c8e00cacae136233f9f9c93b384c35b1a0d
Michal Labowski <michal.labowski@gmail.com>
2019-09-28 11:46:26 +0200

    feat: Add exception method
    Reformat code comments

--- logger.py
+++ logger.py
@@ -2,60 +2,75 @@ import logging
 import sys
 import time
 
-LOGGER_VERSION = 1.1
+LOGGER_VERSION = 1.2
 
 class Logger:
+    """A Logger class definition - it allows to simultanously write data to log file
+    and the console"""
+
     def __init__(self, logger_name='__name__', file_name=' ',
                  file_level=-1, console_level=-1,
-                 formatter='%(created).6f %(name)-12s %(levelname)-8s %(message)s'):
+                 formatter='%(created).6f %(name)-12s %(levelname)-10s %(message)s'):
+        """Initialize the Logger class instance"""
         self.logger = logging.getLogger(logger_name)
-        self.logger.setLevel(logging.DEBUG)
+        # The severity level of the root logger
+        # All events equal and above this level will be logged
+        self.logger.setLevel(logging.DEBUG)  
+        self.initialized = False
         
         if console_level in {logging.DEBUG, logging.INFO, logging.WARNING,
                 logging.ERROR, logging.CRITICAL}:
+            # Initialize the console output only when needed:
             self.console_formatter = logging.Formatter(formatter)
             self.console_level = console_level
             self.init_console_handler()
             self.logger.addHandler(self.console_handler)
+            self.initialized = True
 
-        if file_name is not ' ' and file_level in {logging.DEBUG,
-                logging.INFO, logging.WARNING, logging.ERROR, logging.CRITICAL}:
+        if (file_name is not ' ') and (file_level in {logging.DEBUG,
+                logging.INFO, logging.WARNING, logging.ERROR, logging.CRITICAL}):
+            # Initialize the file output only when nedded:
             self.file_name = file_name
             self.file_level = file_level
             self.file_formatter = logging.Formatter(formatter)
             self.init_file_handler()
             self.logger.addHandler(self.file_handler)
+            self.initialized = True
 
         self.logger.propagate = False
-            
 
     def init_console_handler(self):
+        """ Initialize the console output"""
         self.console_handler = logging.StreamHandler(sys.stdout)
         self.console_handler.setFormatter(self.console_formatter)
         self.console_handler.setLevel(self.console_level)
     
-
     def init_file_handler(self):
+        """Initialize the file output"""
         self.file_handler = logging.FileHandler(self.file_name)
         self.file_handler.setFormatter(self.file_formatter)
         self.file_handler.setLevel(self.file_level)
 
-
     def debug(self, msg, *args, **kwargs):
+        """Set debug message"""
         self.logger.debug(msg, *args, **kwargs)
 
-
     def info(self, msg, *args, **kwargs):
+        """Set info message"""
         self.logger.info(msg, *args, **kwargs)
     
-
     def warning(self, msg, *args, **kwargs):
+        """Set warning message"""
         self.logger.warning(msg, *args, **kwargs)
         
-
     def error(self, msg, *args, **kwargs):
+        """Set error message"""
         self.logger.error(msg, *args, **kwargs)
 
-
     def critical(self, msg, *args, **kwargs):
+        """Set critical message"""
         self.logger.critical(msg, *args, **kwargs)
+
+    def exception(self, msg, *args, **kwargs):
+        """Set exception message"""
+        self.logger.exception(msg, *args, **kwargs)
--- test_logger.py
+++ test_logger.py
@@ -1,5 +1,19 @@
 from logger import Logger
 import logging
 
-my_logger = Logger(logger_name=__name__, console_level=logging.INFO)
-my_logger.info('This it test info message')
+my_logger = Logger(logger_name=__name__, file_name="./test.log",
+                   console_level=logging.INFO, file_level=logging.DEBUG)
+message_number = 1
+my_logger.info('This it a test info message with num %d', message_number)
+my_logger.debug('This it a test debug message %s', str(message_number))
+my_logger.warning('This it a test warning message')
+my_logger.error('This it a test error message')
+my_logger.critical('This it a test critical message')
+
+a = 5
+b = 0
+try:
+    c = a/b
+except Exception as e:
+    my_logger.exception("Exception occured")
+
